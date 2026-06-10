9d7e0a3a9d8d30e04ae9e38a083c144bdbd0d11c
Muhammad Rafi Novandi <mrafinovandi04@gmail.com>
2026-06-10 17:41:49 +0700

    feat: table management

---
+++ realtime-point-of-sales/src/app/(dashboard)/admin/table/page.tsx
@@ -0,0 +1,10 @@
+import { title } from "process";
+import TableManagement from "./_components/table";
+
+export const metadata = {
+  title: "Solitude Cafe | Table Management ",
+};
+
+export default function UserManagementPage() {
+  return <TableManagement />;
+}
